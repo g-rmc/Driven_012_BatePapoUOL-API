78832c974b17ef0550f2e36e38b7648774d7db56
g-rmc <g.rmc3000@gmail.com>
2022-09-02 13:07:07 -0300

    Configurações iniciais do app

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import joi from 'joi';
+import dayjs from 'dayjs';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+    db = mongoClient.db("UOL_API")
+})
+
+/////////
+
+
+app.listen('5000', () => console.log('Listening on 5000'))
