df8514a0ec0992df2f31ac90416b8d738c19dffb
g-rmc <g.rmc3000@gmail.com>
2022-09-02 16:35:31 -0300

    Função POST /participants inserida

--- src/app.js
+++ src/app.js
@@ -17,7 +17,86 @@ mongoClient.connect(() => {
     db = mongoClient.db("UOL_API")
 })
 
-/////////
+const nameSchema = joi.string();
+
+/*
+
+Participante: 
+{
+    name: 'João',
+    lastStatus: 12313123
+}
+
+Mensagem:
+{
+    from: 'João',
+    to: 'Todos',
+    text: 'oi galera',
+    type: 'message',
+    time: '20:04:37'
+}
+
+*/
+
+app.post('/participants', async (req, res) =>{
+
+    const { name } = req.body;
+
+    const validation = nameSchema.validate(name);
+    if(validation.error){
+        res.status(422).send(validation.error.details[0].message);
+        return;
+    }
+
+    /*
+    try {
+        const participants = await db.collection('participants').find().toArray();
+        console.log(participants)
+    } catch (error) {
+        console.log(error)
+    } */
+
+    try {
+        const userSameName = await db.collection('participants').findOne({name});
+        if(userSameName){
+            res.status(409).send("Usuário já cadastrado");
+            return;
+        }
+    } catch (error) {
+        res.status(400).send(error);
+        return;
+    }
+    
+    try {
+        await db.collection('participants').insertOne({
+            name,
+            lastStatus: Date.now()
+        })
+        res.sendStatus(201);
+    } catch (error) {
+        res.status(400).send(error);
+    } 
+
+    try {
+        await db.collection('messages').insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('HH:mm:ss')
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+    /*
+    try {
+        const messages = await db.collection('messages').find().toArray();
+        console.log(messages)
+    } catch (error) {
+        console.log(error)
+    } */
+})
 
 
 app.listen('5000', () => console.log('Listening on 5000'))
